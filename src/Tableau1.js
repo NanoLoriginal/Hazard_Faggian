9f48367488f95bf3b44b30a4f2f57eeddeeccff3
NanoLoriginal <kyliann.faggian@gmail.com>
2022-04-04 15:38:29 +0200

    marche po

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -1,23 +1,60 @@
 class Tableau1 extends Phaser.Scene {
 
-    preload(){
-        this.load.image("starterpack", "assets/tilesheets/platformPack_tilesheet.png");
-        this.load.tilemapTiledJSON("map", "assets/tilemaps/level1.json");
+    preload() {
+        this.load.image('background', 'assets/images/background.png');
+        this.load.image('spike', 'assets/images/spike.png');
+        // At last image must be loaded with its JSON
+        this.load.atlas('player', 'assets/images/kenney_player.png', 'assets/images/kenney_player_atlas.json');
+        this.load.image('tiles', 'assets/tilesets/platformPack_tilesheet.png');
+
+        // Load the export Tiled JSON
+        this.load.tilemapTiledJSON('map', 'assets/tilemaps/Alpha1.json');
     }
 
-    create(){
-        let level1 = this.add.tilemap("map");
-        let terrain = level1.addTilesetImage("starterpack", "terrain");
 
-        //layers
+    create() {
 
-        let groundLayer = level1.createLayer("ground", [terrain], 0, 0);
-        let backgroundLayer = level1.createLayer("background", [terrain], 0, 0);
 
+
+
+
+        const backgroundImage = this.add.image(0, 0, 'background').setOrigin(0, 0);
+        backgroundImage.setScale(1, 0.8);
+        const map = this.make.tilemap({key: 'map'});
+
+        const tileset = map.addTilesetImage('Alpha_test1', 'tiles');
+        this.platforms = map.createStaticLayer('Sol', tileset);
+
+        this.platforms.setCollisionByExclusion(-1, true);
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+
+        this.player = new Player(this)
+
+        this.cameras.main.startFollow(this.player.player,false);
     }
 
-    update(){
+
+    update() {
+
+        switch (true) {
+            case (this.cursors.space.isDown || this.cursors.up.isDown) && this.player.player.body.onFloor():
+                this.player.jump()
+                console.log("oui")
+                break;
+            case this.cursors.left.isDown:
+                this.player.moveLeft()
+                break;
+            case this.cursors.right.isDown:
+                this.player.moveRight();
+                break;
+            default:
+                this.player.stop();
+        }
+
+
+
+
 
     }
-
 }
