8dd566887a965ef1eddbd53f040bc3df2f3a09d1
NanoLoriginal <kyliann.faggian@gmail.com>
2022-04-04 15:31:28 +0200

    tests

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -1,10 +1,18 @@
 class Tableau1 extends Phaser.Scene {
 
     preload(){
-
+        this.load.image("starterpack", "assets/tilesheets/platformPack_tilesheet.png");
+        this.load.tilemapTiledJSON("map", "assets/tilemaps/level1.json");
     }
 
     create(){
+        let level1 = this.add.tilemap("map");
+        let terrain = level1.addTilesetImage("starterpack", "terrain");
+
+        //layers
+
+        let groundLayer = level1.createLayer("ground", [terrain], 0, 0);
+        let backgroundLayer = level1.createLayer("background", [terrain], 0, 0);
 
     }
 
