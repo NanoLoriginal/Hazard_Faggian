0ec942bef53f947dc8863fc17a9c4566088d1e87
Kyliann <92451561+NanoLoriginal@users.noreply.github.com>
2022-06-02 11:39:19 +0200

    map_test json, tileset, modifs dans le tableau pour charger la nouvelle map

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -3,14 +3,15 @@ class Tableau1 extends Phaser.Scene {
 
     preload() {
         this.load.image('background', 'assets/images/background.png');//image de fond
-        this.load.image('spike', 'assets/images/spike.png');//on charge l'image de l'objet piques
+        //this.load.image('spike', 'assets/images/spike.png');//on charge l'image de l'objet piques
         this.load.image('feu', 'assets/images/feu.png');
         // At last image must be loaded with its JSON
         this.load.atlas('player', 'assets/images/kenney_player.png', 'assets/images/kenney_player_atlas.json');//on charge le joueur
-        this.load.image('tiles', 'assets/tilesets/platformPack_tilesheet.png');//on charge le tileset qui contient les éléments utilisés dans tiled
+        //this.load.image('tiles', 'assets/tilesets/platformPack_tilesheet.png');//on charge le tileset qui contient les éléments utilisés dans tiled
+        this.load.image('tiles', 'assets/tilesets/tileset1_test.png');//on charge le tileset qui contient les éléments utilisés dans tiled
 
         // Load the export Tiled JSON
-        this.load.tilemapTiledJSON('map', 'assets/tilemaps/level1.json');
+        this.load.tilemapTiledJSON('map', 'assets/tilemaps/Map1_test.json');
     }
 
 
@@ -22,7 +23,7 @@ class Tableau1 extends Phaser.Scene {
         const map = this.make.tilemap({key: 'map'});
 
         const tileset = map.addTilesetImage('tileset_base', 'tiles');
-        this.platforms = map.createLayer('Platforms', tileset);
+        this.platforms = map.createLayer('sol', tileset);
 
 
         this.cursors = this.input.keyboard.createCursorKeys();
