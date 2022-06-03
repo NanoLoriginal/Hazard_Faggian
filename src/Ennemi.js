78efedc333c9943cb27632702664791a0f5f869f
Kyliann <kyliann.faggian@gmail.com>
2022-06-03 17:57:12 +0200

    update

--- src/Ennemi.js
+++ src/Ennemi.js
@@ -50,7 +50,7 @@ class Ennemi {
         if(this.projectile===false){
             this.projectile = true
             this.scene.time.delayedCall(1150,()=>{
-                this.boule = this.scene.physics.add.sprite(this._ennemi.x,this._ennemi.y, 'feu').setSize(400,400).setDisplaySize(60,60);
+                this.boule = this.scene.physics.add.sprite(this._ennemi.x,this._ennemi.y, 'greenP').setSize(400,400).setDisplaySize(60,60);
                 this.boule.body.setAllowGravity(false);
                 this.scene.physics.moveTo(this.boule, this.scene.player.s.x, this.scene.player.s.y);
                 this.boule.setVelocityX(this.boule.body.velocity.x * 8)
