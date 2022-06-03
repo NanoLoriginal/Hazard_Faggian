45141d64afc3ec33666330e1c8acb3174c7e6edc
Kyliann <kyliann.faggian@gmail.com>
2022-06-03 11:09:58 +0200

    barre de vie et d'énergie check

--- src/UI.js
+++ src/UI.js
@@ -5,11 +5,18 @@ class UI extends Phaser.Scene {
     }
 
     preload() {
+        this.load.image('health','assets/images/barre_de_vie.png');
+        this.load.image('energie','assets/images/barre_energie.png');
 
 
     }
 
     create() {
+        const healthBar = this.add.image(-50,-170,'health').setOrigin(0,0);
+        healthBar.setScale(0.5,0.5);
+
+        const energyBar = this.add.image(0,0,'energie').setOrigin(0,0);
+        energyBar.setScale(0.3,0.3);
 
     }
 
